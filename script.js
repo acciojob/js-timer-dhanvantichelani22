47b530094997a3f1d10482fc1d48db6f545f26f2
Dhanvanti Chelani <90795524+dhanvantichelani22@users.noreply.github.com>
2023-08-02 08:52:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // write js code here if required
+ function currDayTime(){
+const paratag = document.getElementById('timer');
+const date = new Date();
+const day = date.getDate();
+const month = date.getMonth()+1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+
+const text = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+paratag.innerText = text;
+ setTimeout(currDayTime, 1000);
+ }
+currDayTime();
